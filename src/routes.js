cc194487dc387cf80b0651439c931bd3793d99d1
Valentin Kazakov <vkazakov@gmail.com>
2016-09-27 21:55:26 +0300

    Part 1
    Commit for Part 1 Tutorial
    https://valentinmk.gitbooks.io/react-redux-socket-tutorial/content/

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import {
     LoginSuccess,
     Survey,
     NotFound,
+    SocketExamplePage
   } from 'containers';
 
 export default (store) => {
@@ -50,6 +51,7 @@ export default (store) => {
       <Route path="login" component={Login}/>
       <Route path="survey" component={Survey}/>
       <Route path="widgets" component={Widgets}/>
+      <Route path="socketexamplepage" component={SocketExamplePage}/>
 
       { /* Catch all route */ }
       <Route path="*" component={NotFound} status={404} />
