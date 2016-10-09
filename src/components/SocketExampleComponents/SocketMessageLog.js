6a5e38e4abcd44ea936dbd744cb790ced27d2484
Valentin Kazakov <vkazakov@gmail.com>
2016-10-10 00:40:17 +0300

    Commit for Part 5 Tutorial 2
    https://valentinmk.gitbooks.io/react-redux-socket-tutorial/content/Part5.html

--- src/components/SocketExampleComponents/SocketMessageLog.js
+++ src/components/SocketExampleComponents/SocketMessageLog.js
@@ -7,39 +7,53 @@ import * as socketExampleActions from 'redux/modules/socketexamplemodule';
   state => ({
     loaded: state.socketexample.loaded,
     message: state.socketexample.message,
-    connected: state.socketexample.connected}),
+    connected: state.socketexample.connected,
+    message_history: state.socketexample.message_history
+  }),
   socketExampleActions)
 export default class SocketMessageLog extends Component {
   static propTypes = {
     loaded: PropTypes.bool,
     message: PropTypes.string,
-    connected: PropTypes.bool
+    connected: PropTypes.bool,
+    message_history: PropTypes.array,
+    socketsMessageSending: PropTypes.func,
+    socketsMessageReceiving: PropTypes.func
   }
+
+  handleSendButton = (event) => {
+    event.preventDefault();
+    this.props.socketsMessageSending(this.refs.message_text.value);
+    this.props.socketsMessageReceiving(this.refs.message_text.value);
+    this.refs.message_text.value = '';
+  }
+
   render() {
-    const {loaded, message, connected} = this.props;
+    const {loaded, connected, message_history} = this.props;
     return (
       <div>
         <h3>Message log</h3>
         <ul>
-            <li key="1" className="unstyled">
-              <span className="glyphicon glyphicon-arrow-right"> </span>
-              {message}
-            </li>
-            <li key="2" className="unstyled">
-              <span className="glyphicon glyphicon-arrow-left"> </span>
-              [ECHO] {message}
-            </li>
+          {
+            message_history.map((messageHistoryElement, index) =>
+            <li key={index} className={'unstyled'}>
+              <span className={(messageHistoryElement.direction === '->') ? 'glyphicon glyphicon-arrow-right' : 'glyphicon glyphicon-arrow-left'}></span>
+            {messageHistoryElement.message}</li>
+          )}
         </ul>
-        <form className="form-inline">
+        <form
+          className="form-inline"
+          onSubmit={this.handleSendButton}>
           <p></p>
           <div className="form-group">
             <input
             className="form-control input-sm"
             type="text"
-            ref="message_text" readOnly = {(loaded === true) ? false : true}></input>
+            ref="message_text" readOnly = {(loaded && connected === true) ? false : true}></input>
           </div>
           <button
             className="btn btn-primary btn-sm"
+            onClick={this.handleSendButton}
             disabled = {(connected === true) ? false : true}>
             <i className="fa fa-sign-in"/> Send
           </button>
