97a6526020a549f2ddf91370ac70dbc0737f167b
Valentin Kazakov <vkazakov@gmail.com>
2016-11-06 14:30:58 +0300

    Commit 2 for Part2
    https://valentinmk.gitbooks.io/react-redux-socket-tutorial/content/Part2
    .html

--- src/containers/SocketExample/SocketExamplePage.js
+++ src/containers/SocketExample/SocketExamplePage.js
@@ -1,55 +1,18 @@
 import React, {Component} from 'react';
 import Helmet from 'react-helmet';
+import {SocketConnectionLog} from 'components';
+import {SocketMessageLog} from 'components';
 
 export default class SocketExamplePage extends Component {
   render() {
     return (
       <div className="container">
-
         <h1>Socket Exapmle Page</h1>
         <Helmet title="Socket Exapmle Page"/>
         <p>Sockets not connected</p>
-
-        <h3>Socket connection log</h3>
-        <textarea
-          className="form-control"
-          rows="1"
-          readOnly
-          placeholder="Waiting ..."
-          value="
-            index = 2, loaded = true, message = Connected, connected = true
-            index = 1, loaded = false, message = Connecting..., connected = false"/>
-        <button className="btn btn-primary btn-sm">
-          <i className="fa fa-sign-in"/> Connect
-        </button>
-        <button className="btn btn-danger btn-sm">
-          <i className="fa fa-sign-out"/> Disconnect
-        </button>
-        <h3>Message log</h3>
-        <ul>
-            <li key="1" className="unstyled">
-              <span className="glyphicon glyphicon-arrow-right"></span>
-              Socket string
-            </li>
-            <li key="2" className="unstyled">
-              <span className="glyphicon glyphicon-arrow-left"></span>
-              [ECHO] Socket string
-            </li>
-        </ul>
-        <form className="form-inline">
-          <p></p>
-          <div className="form-group">
-            <input
-            className="form-control input-sm"
-            type="text"
-            ref="message_text"></input>
-          </div>
-          <button className="btn btn-primary btn-sm">
-            <i className="fa fa-sign-in"/> Send
-          </button>
-        </form>
+        <SocketConnectionLog />
+        <SocketMessageLog/>
       </div>
-
     );
   }
 }
