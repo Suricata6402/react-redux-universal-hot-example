a473d6a86262f2d2b52c590974e77df9454de5a1
Valentin Kazakov <vkazakov@gmail.com>
2016-12-07 15:38:50 +0300

    Commit 3 for Part3
    https://valentinmk.gitbooks.io/react-redux-socket-tutorial/content/Part3.html

--- src/components/SocketExampleComponents/SocketMessageLog.js
+++ src/components/SocketExampleComponents/SocketMessageLog.js
@@ -1,29 +1,45 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as socketExampleActions from 'redux/modules/socketexamplemodule';
 
+@connect(
+  state => ({
+    loaded: state.socketexample.loaded,
+    message: state.socketexample.message,
+    connected: state.socketexample.connected}),
+  socketExampleActions)
 export default class SocketMessageLog extends Component {
+  static propTypes = {
+    loaded: PropTypes.bool,
+    message: PropTypes.string,
+    connected: PropTypes.bool
+  }
   render() {
+    const {loaded, message, connected} = this.props;
     return (
       <div>
         <h3>Message log</h3>
         <ul>
           <li key="1" className="unstyled">
-            <span className="glyphicon glyphicon-arrow-right"></span>
-            Socket string
+            <span className="glyphicon glyphicon-arrow-right"> </span>
+            {message}
           </li>
           <li key="2" className="unstyled">
-            <span className="glyphicon glyphicon-arrow-left"></span>
-            [ECHO] Socket string
+            <span className="glyphicon glyphicon-arrow-left"> </span>
+            [ECHO] {message}
           </li>
         </ul>
         <form className="form-inline">
           <p></p>
           <div className="form-group">
             <input
-            className="form-control input-sm"
-            type="text"
-            ref="message_text"></input>
+              className="form-control input-sm"
+              type="text"
+              ref="message_text" readOnly = {(loaded === true) ? false : true}></input>
           </div>
-          <button className="btn btn-primary btn-sm">
+          <button
+            className="btn btn-primary btn-sm"
+            disabled = {(connected === true) ? false : true}>
             <i className="fa fa-sign-in"/> Send
           </button>
         </form>
